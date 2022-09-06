f856f64cb6c998ebedf60128be00338c6a2dd7e6
Manikandan D <108661573+manikandandhanen@users.noreply.github.com>
2022-09-06 10:54:17 +0530

    Add files via upload

---
+++ EmployeeDataTable/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmployeesComponent } from './employees/employees.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Ng2OrderModule } from 'ng2-order-pipe';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    Ng2OrderModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ EmployeeDataTable/src/app/shared/employee.model.ts
@@ -0,0 +1,12 @@
+export class Employee{
+    
+    id: number= 0;
+    name: string;
+    email: string;
+    designation: string;
+    department: string;
+    date: any;
+    status: string;
+    salary: any;
+    
+}
